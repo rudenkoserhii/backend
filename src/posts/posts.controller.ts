4828de57580d5c65a5cd9a96e12ca9bd827733cc
rudenkoserhii <rudenko.sergey@ukr.net>
2023-10-18 15:16:06 +0300

    one more

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,21 +1,16 @@
-import {
-  Body,
-  Controller,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
+import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Post } from './posts.model';
 
 @Controller('posts')
 export class PostsController {
   constructor(private postService: PostsService) {}
 
-  @Post()
-  @UseInterceptors(FileInterceptor('image'))
-  createPost(@Body() dto: CreatePostDto, @UploadedFile() image) {
-    return this.postService.create(dto, image);
+  @ApiOperation({ summary: 'Get all posts' })
+  @ApiResponse({ status: 200, type: [Post] })
+  @Get()
+  getAll() {
+    return this.postService.getAllPosts();
   }
 }
