6c67c9f3c415bcc264dfa5c745926ed4000f39fc
rudenkoserhii <rudenko.sergey@ukr.net>
2023-10-19 03:00:19 +0300

    +

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from './pipes/validation.pipe';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.enableCors({
     allowedHeaders: ['content-type'],
