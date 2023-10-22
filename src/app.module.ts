f1a74e104960a106661ad8c777692da7bd16201f
rudenkoserhii <rudenko.sergey@ukr.net>
2023-10-22 17:20:33 +0300

    fix env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,34 +6,31 @@ import { User } from './user/user.model';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { Post } from './posts/posts.model';
+import { Dialect } from 'sequelize';
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.${process.env.NODE_ENV}.env`,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     SequelizeModule.forRoot({
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: true,
-      },
-      host: process.env.POSTGRES_HOST || 'localhost',
-      port: Number(process.env.POSTGRESS_PORT) || 5432,
-      username: process.env.POSTGRES_USER || 'postgres',
-      password: process.env.POSTGRESS_PASSWORD || 'root',
-      database: process.env.POSTGRES_DB || 'test_zenbit',
+      dialect: <Dialect>'postgres',
+      protocol: 'postgres',
+      ssl: Boolean(process.env.SSL),
+      // dialectOptions: {
+      //   ssl: false,
+      // },
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRESS_PORT),
+      username: process.env.POSTGRES_USER,
+      password: String(process.env.POSTGRESS_PASSWORD),
+      database: String(process.env.POSTGRES_DB),
       models: [User, Post],
       autoLoadModels: true,
-      // host: 'localhost',
-      // port: 5432,
-      // username: 'postgres',
-      // password: 'root',
-      // database: 'test_zenbit',
-      // models: [User, Post],
-      // autoLoadModels: true,
     }),
+
     UserModule,
     AuthModule,
     PostsModule,
