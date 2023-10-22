c308974cfc4942185e165c4d90af1ae1d118df0b
rudenkoserhii <rudenko.sergey@ukr.net>
2023-10-22 19:05:56 +0300

    return user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,7 +42,7 @@ export class AuthService {
     const payload = { email: user.email, id: user.id };
     return {
       token: this.jwtService.sign(payload),
-      email: user.email,
+      user: user,
     };
   }
 
